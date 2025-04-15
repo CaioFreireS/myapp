f1daba228054631ac71781844bdac7a61f9d1d59
caiofreiresilva2305 <caiofreiresilva2305@gmail.com>
2025-04-15 16:54:15 +0000

    fit: splash_screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,12 +1,55 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/app_colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: Stack(
+        children: [
+          Image.asset('assets/banners/banner_splash.png'),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 32,
+                children: [
+                  Image.asset('assets/logo.png', width: 192),
+                  Column(
+                    children: [
+                      Text(
+                        "Um parceiro inovador para sua",
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      ),
+                      Text(
+                        "melhor experiência culinária!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 22,
+                          color: AppColors.mainColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Bora!"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
